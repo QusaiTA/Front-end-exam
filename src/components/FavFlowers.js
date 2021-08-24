02c662a6ee305fa5daefbab1a696998b10c18640
QusaiTA <tashtoshqusai@gmail.com>
2021-08-24 13:57:03 +0300

    70%solved

--- src/components/FavFlowers.js
+++ src/components/FavFlowers.js
@@ -1,14 +1,142 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
+import { Card,Button } from 'react-bootstrap';
+import{Form} from 'react-bootstrap';
 
 class FavFlowers extends React.Component {
+
+  constructor(props){
+
+    super(props);
+    this.state = {
+      FavFlower :[],
+      index : -1,
+      flowerName :'',
+      flowerObj:{},
+      showForm:false
+    }
+  }
+
+
+  componentDidMount() {
+
+    const {user} = this.props.auth0;
+    axios
+      .get('http://localhost:3003/getFavFlowers', {params : {userEmail:user.email}})
+      .then(result => {
+        this.setState({
+          FavFlower: result.data,
+        })
+      }).catch(err => { console.log(err); })
+
+
+  }
+
+  deleteFlower = (idx) => {
+
+   const{user} = this.props.auth0;
+   axios
+   .delete(`http://localhost:3003/deleteFlowers/${idx}`, {params: {userEmail:user.email}})
+   .then(result => {
+    this.setState({
+      FavFlower: result.data,
+    })
+}).catch(err => {console.log(err);})
+}
+
+ updateFlower = (flower,idx) => {
+   this.setState({
+     index:idx,
+     flowerName:flower.name,
+     showForm:true,
+     flowerObj:flower
+     
+   })
+
+
+
+
+ }
+
+ updateFormSubmit = (event) => {
+
+  event.preventDefault();
+  const{user} = this.props.auth0;
+  const params = {
+    userEmail:user.email,
+    flowerObj:{
+      flowerName: event.target.flowerName.value,
+      flowerImg: this.state.flowerObj.photo
+    }
+      
+  }
+
+  axios
+  .put(`http://localhost:3003/UpdateFlowers/${this.state.index}`,params)
+  .then(result => {
+    this.setState({
+      FavFlower: result.data,
+    })
+  }).catch(err => {console.log(err);})
+
+
+ }
+
+
   render() {
     return(
       <>
+
+
+      {this.state.showForm &&
+      <Form onSubmit={e => this.updateFormSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Flower Name</Form.Label>
+        <Form.Control type="text" name='flowerName' defaultValue={this.state.flowerName} />
+        
+      </Form.Group>
+    
+      <Button variant="primary" type="submit">
+        Update
+      </Button>
+    </Form>
+      
+  }
         <h1>My Favorite Flowers</h1>
+        {this.state.FavFlower.length && this.state.FavFlower.map((favFlower,idx) => {
+          return (
+
+            <>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={favFlower.photo} />
+                <Card.Body>
+                  <Card.Title>{favFlower.name}</Card.Title>
+                  <Card.Text>
+                    {favFlower.instructions}
+                  </Card.Text>
+                  <Button onClick ={ () => {this.deleteFlower(idx)}} variant="danger">Delete</Button>
+                  <Button onClick ={ () => {this.updateFlower(favFlower,idx)}} variant="primary">Update</Button>
+                </Card.Body>
+              </Card>
+
+
+            </>
+
+
+          )
+          
+        
+        
+          })}
+
+
       </>
     )
-  }
+  
 }
 
-export default FavFlowers;
+}
+
+export default withAuth0(FavFlowers);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,82 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Card,Button } from 'react-bootstrap';
+import { withAuth0 } from '@auth0/auth0-react'
 
 class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allFlowers: [],
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3003/getAllFlowers')
+      .then(result => {
+        this.setState({
+          allFlowers: result.data,
+        })
+      }).catch(err => { console.log(err); })
+
+
+  }
+
+  addFlower = (FlowerObj) => {
+    const {user} = this.props.auth0;
+
+    const params = {
+      userEmail:user.email,
+      FlowerObj:{
+        FlowerName : FlowerObj.name,
+        FlowerImg : FlowerObj.photo,
+        FlowerDes : FlowerObj.instructions
+      }
+      }
+    
+      axios
+      .post('http://localhost:3003/addFlowersToFav',params)
+      .catch(err => {console.log(err)})
+    }
+  
+
+
+
 
   render() {
     return (
       <>
         <h1>API Flowers</h1>
+        {this.state.allFlowers.length && this.state.allFlowers.map((flower, idx) => {
+
+          return (
+            <>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={flower.photo} />
+                <Card.Body>
+                  <Card.Title>{flower.name}</Card.Title>
+                  <Card.Text>
+                    {flower.instructions}
+                  </Card.Text>
+                  <Button onClick ={ () => {this.addFlower(flower)}}variant="primary">ADD TO FAV</Button>
+                </Card.Body>
+              </Card>
+
+
+            </>
+
+
+          )
+
+
+        })}
       </>
     )
   }
 }
 
-export default Home;
+export default withAuth0(Home);
